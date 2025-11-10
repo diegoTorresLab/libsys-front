af8e097271d34f632d71d20d2a860ae7aeef509f
Diego Torres <diegotorresperez14@gmail.com>
2025-11-10 12:50:06 -0600

    Header implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    }
+];
