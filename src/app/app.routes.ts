9dd261bf09ca8eec01449311279600f38d91825c
Diego Torres <diegotorresperez14@gmail.com>
2025-11-10 14:21:12 -0600

    Not Found fallback screen implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { NothingSelectedComponent } from './nothing-selected/nothing-selected.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: NothingSelectedComponent,
         title: 'LibSys - Inicio'
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
