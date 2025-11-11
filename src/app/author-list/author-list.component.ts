ee2ebabc56c630ca28b8d1d9d3703e04b0805dd4
Diego Torres <diegotorresperez14@gmail.com>
2025-11-11 11:18:32 -0600

    Autor-List lists the authors (you can't upgrade or delete authors yet)

--- src/app/author-list/author-list.component.ts
+++ src/app/author-list/author-list.component.ts
@@ -1,11 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Author } from './author.model';
+import { AuthorService } from './author.service';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
+import { Toast } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { Ripple } from 'primeng/ripple';
 
 @Component({
   selector: 'app-author-list',
-  imports: [],
+  imports: [
+    TableModule,
+    InputTextModule,
+    CommonModule,
+    ButtonModule,
+    RouterModule,
+    Toast,
+  ],
+  providers: [MessageService],
   templateUrl: './author-list.component.html',
-  styleUrl: './author-list.component.css'
+  styleUrl: './author-list.component.css',
 })
-export class AuthorListComponent {
+export class AuthorListComponent implements OnInit {
+  author: Author[] = [];
 
+  constructor(
+    private authorService: AuthorService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAuthors();
+  }
+
+  getAuthors() {
+    this.authorService.getAuthors().subscribe({
+      next: (data) => {
+        this.author = data;
+      },
+      error: (err) => {
+        console.log(err);
+        this.showError();
+      },
+    });
+  }
+
+  showError() {
+    this.messageService.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'Error al obtener autores',
+      life: 3000,
+    });
+  }
+
+  updateAuthors(id: string) {
+    // TODO: Implement update logic, perhaps navigate to edit form
+    console.log('Update author with id:', id);
+  }
+
+  deleteAuthors(id: string) {
+    this.authorService.deleteAuthors(id).subscribe({
+      next: () => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Éxito',
+          detail: 'Autor eliminado correctamente',
+          life: 3000,
+        });
+        this.getAuthors(); // Refresh the list
+      },
+      error: (err) => {
+        console.log(err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al eliminar autor',
+          life: 3000,
+        });
+      },
+    });
+  }
 }
